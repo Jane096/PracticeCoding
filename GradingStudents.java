1299fa110126a98e77d6fc8c4ab122a069f6f01d
Lee JE <58355531+Jane096@users.noreply.github.com>
2020-04-29 19:55:46 +0900

    Create GradingStudents.java

---
+++ GradingStudents.java
@@ -0,0 +1,27 @@
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> answer = new ArrayList<Integer>();
+
+        for(int i=0; i<grades.size(); i++) {
+            
+            if(grades.get(i) < 38){
+                answer.add(grades.get(i));
+            }else if(grades.get(i) % 5 >= 3) {
+                answer.add(grades.get(i)+(5-grades.get(i)%5));
+            }else{
+                answer.add(grades.get(i));
+            }
+        }
+        Collections.sort(answer);
+        Collections.reverse(answer);
+        return answer;
+    }
+}
